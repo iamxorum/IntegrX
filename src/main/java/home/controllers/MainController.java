956e386caa40294c9a1df794dec75e70c7ae529b
Xorum <andrei.smurariu@gmail.com>
2024-03-19 02:07:42 +0200

    19.03.2024 - Created Main Form {UI}

--- src/main/java/home/controllers/MainController.java
+++ src/main/java/home/controllers/MainController.java
@@ -2,9 +2,8 @@ package home.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
-public class Controller {
+public class MainController {
 	@FXML
 	protected void onCloseButtonClicked(ActionEvent event) {
 		System.exit(0);
