0e7dc9ba74dc458a31cb458edd32af3012100333
Xorum <andrei.smurariu@gmail.com>
2024-03-19 13:26:06 +0200

    19.03.2024_pt2 - Fixed some GUI bugs in the form
    Added also some transition in the toggle buttons.

--- src/main/java/home/controllers/MainController.java
+++ src/main/java/home/controllers/MainController.java
@@ -2,11 +2,31 @@ package home.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 public class MainController {
+	@FXML
+	private ToggleButton rectangularButton;
+
+	@FXML
+	private ToggleButton trapezoidalButton;
+
+	@FXML
+	private ToggleButton simpsonButton;
+
+	@FXML
+	private void initialize() {
+		ToggleGroup toggleGroup = new ToggleGroup();
+		rectangularButton.setToggleGroup(toggleGroup);
+		trapezoidalButton.setToggleGroup(toggleGroup);
+		simpsonButton.setToggleGroup(toggleGroup);
+	}
+
 	@FXML
 	protected void onCloseButtonClicked(ActionEvent event) {
 		System.exit(0);
 	}
+
 
 }
